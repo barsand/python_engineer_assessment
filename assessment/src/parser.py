b80af251504d33afddd82b9c37ad68f6bd3e88b0
Lucas Barsand <barsandlucas@gmail.com>
2022-08-22 13:15:25 -0300

    feat: add function to insert csv data to db

--- assessment/src/parser.py
+++ assessment/src/parser.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import sqlalchemy
 from . import utils
 
 
@@ -9,7 +10,25 @@ INTEREST_KEYS = ["Interest1", "Interest2", "Interest3", "Interest4"]
 class PeopleCSVParser:
     def __init__(self, csv_path):
         self.read_csv(csv_path)
-        self.clean_data()
+
+        engine = sqlalchemy.create_engine("mysql://root:root@127.0.0.1/main")
+        connection = engine.connect()
+        metadata = sqlalchemy.schema.MetaData(engine)
+
+        self.db_info = {
+            "engine": engine,
+            "connection": connection,
+            "metadata": metadata,
+            "interests_tb": sqlalchemy.schema.Table(
+                "Interests", metadata, autoload=True, autoload_with=engine
+            ),
+            "people_tb": sqlalchemy.schema.Table(
+                "People", metadata, autoload=True, autoload_with=engine
+            ),
+            "people_interests_tb": sqlalchemy.schema.Table(
+                "PeopleInterests", metadata, autoload=True, autoload_with=engine
+            ),
+        }
 
     def _json(self, obj=None, sort_keys=True, indent=4, **kwargs):
         if obj is None:
@@ -43,10 +62,44 @@ class PeopleCSVParser:
             if not person_interests:
                 continue
 
+            curr_city = utils.normalize_spacing(entry['City'])
+            curr_city = utils.remove_accentuation(curr_city)
+
             self.people.append({
-                'Name': entry['Name'],
+                'Name': utils.normalize_spacing(entry['Name']),
                 'Age': entry['Age'],
-                'City': entry['City'],
+                'City': curr_city,
                 'PhoneNumber': utils.normalize_phone_number(entry['PhoneNumber']),
                 'Interests': person_interests,
             })
+
+    def to_db(self):
+        db_info = self.db_info
+        db_info["connection"].execute(db_info["interests_tb"].delete())
+        db_info["connection"].execute(db_info["people_tb"].delete())
+        db_info["connection"].execute(db_info["people_interests_tb"].delete())
+
+        self.interest_slug2db_ref = dict()
+        for interest in self.interests:
+            res = db_info["connection"].execute(db_info["interests_tb"].insert().values({
+                "slug": interest
+            }))
+            self.interest_slug2db_ref[interest] = res.inserted_primary_key[0]
+
+        for person in self.people:
+            res = db_info["connection"].execute(db_info["people_tb"].insert().values(**{
+                "name": person["Name"],
+                "age": person["Age"],
+                "phone_number": person["PhoneNumber"],
+                "city": person["City"],
+            }))
+            person["db_ref"] = res.inserted_primary_key[0]
+
+
+            for interest in person["Interests"]:
+                res = db_info["connection"].execute(
+                    db_info["people_interests_tb"].insert().values(**{
+                        "person_id": person["db_ref"],
+                        "interest_id": self.interest_slug2db_ref[interest],
+                    })
+                )
--- assessment/tests/test_parser.py
+++ assessment/tests/test_parser.py
@@ -1,10 +1,11 @@
 import json
+import sqlalchemy
 from src import parser
 from base import BaseTest
 
 
 class TestParser(BaseTest):
-    def _test_csv2json(self):
+    def test_csv2json(self):
         kwargs = {
             "indent": 4,
             "sort_keys": True,
@@ -20,9 +21,29 @@ class TestParser(BaseTest):
 
     def test_csv_clean(self):
         csv_parser = parser.PeopleCSVParser('./tests/data/people.csv')
-        self.assertEqual(len(csv_parser.people), 9)
+        csv_parser.clean_data()
+        self.assertEqual(len(csv_parser.people), 4)
         for p in csv_parser.people:
             self.assertEqual(
                 sorted(p.keys()),
                 ['Age', 'City', 'Interests', 'Name', 'PhoneNumber']
+            )
+
+    def test_db_insert(self):
+        csv_parser = parser.PeopleCSVParser('./tests/data/people.csv')
+        csv_parser.clean_data()
+        csv_parser.to_db()
+
+        expected_rows = {
+            "people_tb": 4,
+            "interests_tb": 13,
+            "people_interests_tb": 14,
+        }
+
+        for k, v in expected_rows.items():
+            self.assertEqual(
+                len(csv_parser.db_info["connection"].execute(
+                    sqlalchemy.sql.select([csv_parser.db_info[k]])
+                ).fetchall()),
+                v
             )
